cc8b9186171e6cbe0019434579bb55e76ad362c8
Verneri <vniinima18@student.oulu.fi>
2024-10-28 17:08:37 +0200

    us1 test4

--- bowling.py
+++ bowling.py
@@ -8,6 +8,8 @@ class BowlingGame:
         self._frames = []
     
     def add_frame(self, frame: Frame) -> None:
+        if len(self._frames) == 10:
+            raise BowlingError()
         self._frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
