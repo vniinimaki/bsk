4cdc1567b2362ad27a0c0881c3c7f50fd28b4f98
Verneri <vniinima18@student.oulu.fi>
2024-10-28 18:50:04 +0200

    us8 test1

--- bowling.py
+++ bowling.py
@@ -6,10 +6,10 @@ class BowlingGame:
 
     def __init__(self):
         self._frames = []
-    
+
     def add_frame(self, frame: Frame) -> None:
         if len(self._frames) == 10:
-            raise BowlingError()
+            raise BowlingError
         self._frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
@@ -19,24 +19,34 @@ class BowlingGame:
 
     def calculate_score(self) -> int:
         score = 0
-        is_spare = False
-        is_strike = False
-        for frame in self._frames:
-            if is_strike:
-                score = score + frame.get_first_throw() + frame.get_second_throw()
-                is_strike = False
-            if is_spare:
-                score = score + frame.get_first_throw()
-                is_spare = False
-            if frame.is_spare:
-                is_spare = True
-            if frame.is_strike:
-                is_strike = True
+        for i, frame in enumerate(self._frames):
+            if frame.is_spare():
+                if i == len(self._frames) - 1:
+                    frame.set_bonus(self._bonus_throw)
+                else:
+                    frame.set_bonus(self._frames[i + 1].get_first_throw())
+            if frame.is_strike():
+                if i == len(self._frames) - 1:
+                    frame.set_bonus(self._bonus_throw + self._second_bonus_throw)
+                else:
+                    if i == len(self._frames) - 2:
+                        if self._frames[i + 1].is_strike():
+                            frame.set_bonus(
+                                self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw() +
+                                self._bonus_throw)
+                        else:
+                            frame.set_bonus(
+                                self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw())
+                    elif self._frames[i + 1].is_strike():
+                        frame.set_bonus(self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw() +
+                                        self._frames[i + 2].get_first_throw())
+                    else:
+                        frame.set_bonus(self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw())
             score = score + frame.score()
         return score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        self._bonus_throw = bonus_throw
 
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        self._second_bonus_throw = bonus_throw
