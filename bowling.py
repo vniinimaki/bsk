c5f7d548aa69196a283a1fd499b22d6cc60a67ae
Verneri <vniinima18@student.oulu.fi>
2024-10-28 17:51:14 +0200

    us2 test2

--- bowling.py
+++ bowling.py
@@ -19,8 +19,20 @@ class BowlingGame:
 
     def calculate_score(self) -> int:
         score = 0
+        is_spare = False
+        is_strike = False
         for frame in self._frames:
-            score += frame.score()
+            if is_strike:
+                score = score + frame.get_first_throw() + frame.get_second_throw()
+                is_strike = False
+            if is_spare:
+                score = score + frame.get_first_throw()
+                is_spare = False
+            if frame.is_spare:
+                is_spare = True
+            if frame.is_strike:
+                is_strike = True
+            score = score + frame.score()
         return score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
